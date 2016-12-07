59a23b7b72a2e177f4169f97d8f2b4fa1beaf13a
Dave Cuza <dave.cuza90@gmail.com>
2016-12-06 20:51:55 -0500

    Log class added and some work at the analizer begined

---
+++ SquidLogsAnalizer/Log.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SquidLogsAnalizer
+{
+    public class Log
+    {
+        private string _username;
+        private string _hostIp;
+        private string _httpMethod;
+        private string _statusCode;
+        private string _destinationHost;
+        private string _squidAction;
+        private string _destinationPort;
+        private string _mimeType;
+
+        public Log(string username, string hostIp, string httpMethod, string statusCode, string destinationHost,
+            string squidAction, string destinationPort, string mimeType)
+        {
+            _username = username;
+            _hostIp = hostIp;
+            _httpMethod = httpMethod;
+            _statusCode = statusCode;
+            _destinationHost = destinationHost;
+            _squidAction = squidAction;
+            _destinationPort = destinationPort;
+            _mimeType = mimeType;
+        }
+
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        public string HostIp
+        {
+            get { return _hostIp; }
+            set { _hostIp = value; }
+        }
+
+        public string HttpMethod
+        {
+            get { return _httpMethod; }
+            set { _httpMethod = value; }
+        }
+
+        public string StatusCode
+        {
+            get { return _statusCode; }
+            set { _statusCode = value; }
+        }
+
+        public string DestinationHost
+        {
+            get { return _destinationHost; }
+            set { _destinationHost = value; }
+        }
+
+        public string SquidAction
+        {
+            get { return _squidAction; }
+            set { _squidAction = value; }
+        }
+
+        public string DestinationPort
+        {
+            get { return _destinationPort; }
+            set { _destinationPort = value; }
+        }
+
+        public string MimeType
+        {
+            get { return _mimeType; }
+            set { _mimeType = value; }
+        }
+
+        public static Log GetLogFromLine(String line)
+        {
+            line.Trim();
+            string[] logline = line.Split(' ');
+            List<string> logElements = new List<string>();
+            foreach (var element in logline)
+            {
+                if (element != "")
+                {
+                    logElements.Add(element);
+                }
+            }
+            Log log = new Log(
+                logElements[7],
+                logElements[2],
+                logElements[5],
+                logElements[3],
+                logElements[6],
+                logElements[8],
+                logElements[6],
+                logElements[9]);
+
+            return log;
+        }
+    }
+}
--- SquidLogsAnalizer/Program.cs
+++ SquidLogsAnalizer/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace SquidLogsAnalizer
 {
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-		}
-	}
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            TextReader tr = new StreamReader("access.log");
+            string line = tr.ReadLine();
+            Log log = Log.GetLogFromLine(line);
+            if (log.Username != "")
+            {
+                Console.WriteLine(log.Username + " " + log.HostIp + " " + log.HttpMethod + " " +
+                                  log.DestinationHost + " " + log.SquidAction);
+            }
+
+
+            while (line != null)
+            {
+                line = tr.ReadLine();
+                if (line != null)
+                {
+                    log = Log.GetLogFromLine(line);
+                    if (log.Username != "")
+                    {
+                        Console.WriteLine(log.Username + " " + log.HostIp + " " + log.HttpMethod + " " +
+                                          log.DestinationHost + " " + log.SquidAction);
+                    }
+                }
+            }
+            tr.Close();
+        }
+    }
 }
