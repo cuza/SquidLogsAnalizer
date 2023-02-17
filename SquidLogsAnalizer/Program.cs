5f8feb5336679b0f930b344ee914a1fe0310ebb7
Manuel Alberto Gorrin <manuelalberto.gorrin@gmail.com>
2023-02-16 21:20:26 -0500

    code migrated to .net7.0

--- SquidLogsAnalizer/Program.cs
+++ SquidLogsAnalizer/Program.cs
@@ -15,7 +15,7 @@ namespace SquidLogsAnalizer
             }
             else
             {
-                ProcessLog("./access.log");
+                ProcessLog("./save.log");
             }
         }
 
